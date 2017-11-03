2e32fe8a404d8dd8b3389d5d05fef8817e9a1a28
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-11-03 20:20:48 +0000

    dcos cloud image build scripts modified for yapster

--- cloud_images/centos7/create_dcos_ami.sh
+++ cloud_images/centos7/create_dcos_ami.sh
@@ -2,15 +2,15 @@
 set -o errexit -o nounset -o pipefail
 
 # AWS profile with appropriate credentials for Packer to create the AMI
-export AWS_PROFILE=${AWS_PROFILE:-"development"}
+export AWS_PROFILE=${AWS_PROFILE:-"root"}
 
 # Base CentOS 7 AMI and region
-export SOURCE_AMI=${SOURCE_AMI:-"ami-31a8ca51"}
-export SOURCE_AMI_REGION=${SOURCE_AMI_REGION:-"us-west-2"}
+export SOURCE_AMI=${SOURCE_AMI:-"ami-785cfc01"}
+export SOURCE_AMI_REGION=${SOURCE_AMI_REGION:-"eu-west-1"}
 # Version upgraded to in install_prereqs.sh
 export CENTOS_VERSION=${CENTOS_VERSION:-"7.4.1708"}
 # Comma separated string of AWS regions to copy the resulting DC/OS AMI to
-export DEPLOY_REGIONS=${DEPLOY_REGIONS:-"us-west-2"}
+export DEPLOY_REGIONS=${DEPLOY_REGIONS:-"eu-west-1"}
 
 # Useful options include -debug and -machine-readable
 PACKER_BUILD_OPTIONS=${PACKER_BUILD_OPTIONS:-""}
